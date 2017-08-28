51963a45e0fd624f0c76db668c6492a004d5a98a
BangKho <BangKho@BANGKHO-PC>
2017-08-28 14:26:10 +0700

    Menambahkan Latihan2 : Person.java Employ.java KonstruktorSuperKelas.java

---
+++ src/Latihan3/Hewan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan3;
+
+/**
+ *
+ * @author BangKho
+ */
+public class Hewan {
+    public static void testClassMethod(){
+        System.out.println("This Class Method is Hewan...");
+    }
+    public void testInstanceMethod(){
+        System.out.println("This Instance Method is Hewan...");
+    }
+}
