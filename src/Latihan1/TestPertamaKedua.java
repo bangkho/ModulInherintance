d67fafa531e2ab23892b17316584d39c703acdb4
BangKho <>
2017-08-28 13:50:27 +0700

    Penambahan Latihan1 : Pertama.java Kedua.java Ketiga.java

---
+++ src/Latihan1/TestPertamaKedua.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1;
+
+/**
+ *
+ * @author BangKho
+ */
+public class TestPertamaKedua {
+    public static void main(String[]args){
+        Kedua S2 = new Kedua();
+        S2.BacaSuper();
+        S2.info();
+        System.out.println();
+        Pertama S1 = new Pertama();
+        S1.protek();
+        S1.info();
+    }
+}
