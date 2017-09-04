d9f7f05dfe364388e91f40a287c35f84631b84d0
BangKho <BangKho@BANGKHO-PC>
2017-09-04 07:54:22 +0700

    Menambahkan Latihan 3 : Gajah.java Hewan.java

---
+++ src/Latihan3/Hewan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan3;
+
+/**
+ *
+ * @author BangKho
+ */
+public class Hewan {
+    public static void testClassMethod(){
+        System.out.println("This Class Method in Hewan...");
+    }
+    public void testInstanceMethod(){
+        System.out.println("This Instance Method in Hewan...");
+    }
+}
